b12db96310a014eb9ab9600a213b4878598ece59
Zachary Collins <zacharyjacobcollins@gmail.com>
2016-03-31 19:28:43 -0400

    updates

--- main.go
+++ main.go
@@ -3,17 +3,16 @@ package main
 import (
 	"net/http"
 	"github.com/gorilla/mux"
-	"github.com/zacharyjacobcollins/GroupOrganization/handlers"
+	"github.com/ZacharyJacobCollins/GroupOrganization/handlers"
 )
 
 func main() {
 	var r = mux.NewRouter()
-	r.HandleFunc("/", handlers.IndexPageHandler)
-	r.HandleFunc("/internal", InternalPageHandler)
-
-	r.HandleFunc("/login", LoginHandler).Methods("POST")
-	r.HandleFunc("/logout", LogoutHandler).Methods("POST")
-
-	http.Handle("/", r)
-	http.ListenAndServe(":8000", nil)
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
+	http.Handle("/",  http.FileServer(http.Dir("./html")))
+	
+	r.HandleFunc("/internal", handlers.InternalPageHandler)
+	r.HandleFunc("/login", handlers.LoginHandler).Methods("POST")
+	r.HandleFunc("/logout", handlers.LogoutHandler).Methods("POST")
+	http.ListenAndServe(":1337", r)
 }
